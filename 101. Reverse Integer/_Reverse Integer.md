61e1da432329e13b86549cfb13f1eed80060653a
Kai Zhang <main@kai.sh>
2019-09-28 00:05:19 +0200

    解决可能溢出问题

--- 101. Reverse Integer/_Reverse Integer.md
+++ 101. Reverse Integer/_Reverse Integer.md
@@ -15,7 +15,9 @@ Input: 120
 Output: 21
 
 # Two sum notes:
-没有特别的笔记 挺简单的？
+没有特别的笔记 挺简单的？（打脸了，没考虑溢出）
+1. MAX_INT = 2,147,483,647
+但其反整数 R_MAX_INT (746,384,741,2)>>MAX_INT 不能够储存在int中 
 
 # Appendix
 https://leetcode.com/problems/reverse-integer/
--- 101. Reverse Integer/k_Reverse Integer.cpp
+++ 101. Reverse Integer/k_Reverse Integer.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-int input = -321;
+int input = 120;
 
 int reverse(int x) {
     int result = 0;
     int quotient = x;
-    int reminder;
+    int reminder = 0;
     do{
         reminder = quotient % 10;
         quotient = quotient / 10;
+        if (result > INT_MAX/10 || (result == INT_MAX / 10 && quotient > 7)) return 0;
+        if (result < INT_MIN/10 || (result == INT_MIN / 10 && quotient < -8)) return 0;
         result = result * 10 + reminder;
     }while((abs(quotient) > 0));
     return result;
