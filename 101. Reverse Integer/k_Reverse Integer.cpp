0ea1f382179374f703f6f36a1abeecb25d9bfa6a
Kai Zhang <main@kai.sh>
2019-09-27 23:48:55 +0200

    Update k_Reverse Integer.cpp
    不应该用余数判断应该用商判断

--- 101. Reverse Integer/k_Reverse Integer.cpp
+++ 101. Reverse Integer/k_Reverse Integer.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int input = 123;
+int input = -321;
 
 int reverse(int x) {
     int result = 0;
@@ -9,10 +9,8 @@ int reverse(int x) {
     do{
         reminder = quotient % 10;
         quotient = quotient / 10;
-        if(reminder == 0) break;
         result = result * 10 + reminder;
-    }while(reminder != 0);
-    std::cout<<result<<std::endl;
+    }while((abs(quotient) > 0));
     return result;
 }
 
